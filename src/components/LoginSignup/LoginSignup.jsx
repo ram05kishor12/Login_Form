b2eedf69e072ab1c8c6e6ead32dc0501e5e4dba6
ram05kishor12 <ramkishore29861@gmail.com>
2023-10-26 23:23:10 +0530

    second commit

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -4,23 +4,14 @@ import './LoginSignup.css'
 import email_icon from'../Assests/email.png'
 import password_icon from'../Assests/password.png'
 import person_icon from'../Assests/person.png'
-import { database } from '../Firebase/firebsecnfig'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+
 
 
 
 export default function LoginSignup  () {
   const [action, setAction] = useState('Sign Up');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-
-    createUserWithEmailAndPassword(database, email, password).then(data => {
-      console.log(data,"authData");
-    })
-  }
+ 
 
  
   return ( 
@@ -47,8 +38,8 @@ export default function LoginSignup  () {
         
         {action === "Sign"?<div></div>:<div className="forgot-password">Lost Password? <span>Click here!</span></div>}
         <div className="submit-container">
-            <form onsubmit={(e)=>handleSubmit(e)}><div className={action ==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}><button>Sign Up</button></div></form>
-            <form onsubmit={(e)=>handleSubmit(e)}><div className={action ==="Sign Up"?"submit gray":"submit"} onClick={ ()=>{setAction("Login")}} ><button >Login</button></div></form>
+            <div className={action ==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}><button>Sign Up</button></div>
+            <div className={action ==="Sign Up"?"submit gray":"submit"} onClick={ ()=>{setAction("Login")}} ><button >Login</button></div>
         </div>
 
 
